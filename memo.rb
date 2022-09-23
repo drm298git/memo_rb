298b42bf47506d4131cdc335969e4dd6e3e29260
drm298git <hiroki.hayase.0608@gmail.com>
2022-09-23 18:31:13 +0900

    メモの編集機能を修正

--- memo.rb
+++ memo.rb
@@ -1,39 +1,44 @@
 require "csv" # CSVファイルを扱うためのライブラリを読み込んでいます
 
-puts "1 → 新規でメモを作成する / 2 → 既存のメモを編集する"
+print "1 → 新規でメモを作成する / 2 → 既存のメモを編集する / 3 → 終了する" 
 
 memo_type = gets.to_i # ユーザーの入力値を取得し、数字へ変換しています
-
-# if文を使用して続きを作成していきましょう。
-# 「memo_type」の値（1 or 2）によって処理を分岐させていきましょう。
 
 #1と2の入力による条件分岐
 if memo_type == 1
 #新規メモの作成
-p "新規でメモを作成します。拡張子なしで名前を入力してください"
+print "新規でメモを作成します。拡張子なしで名前を入力してください"
 file_name = gets.chomp
 
-p "メモの内容を記入して下さい。Ctrl+Dで保存します"
+print "メモの内容を記入して下さい。Ctrl+Dで保存します"
 imput_memo = STDIN.read
 memo = imput_memo.chomp
 
 CSV.open("#{file_name}.csv","w") do |csv|
-p "[#{memo}]"
+CSV.puts "[#{memo}]"
 
 end
 
+
 elsif memo_type == 2
-p "保存されているメモを編集します。拡張子なしで名前を入力してください"   #既存メモの編集
+#既存メモの編集
+print "保存されているメモを編集します。拡張子なしで名前を入力してください" 
 file_name = gets.chomp
 
-p "メモの内容を編集して下さい。Ctrl+Dで保存します"
+print "メモの内容を編集して下さい。Ctrl+Dで保存します"
 edit_memo = STDIN.read
 memo = edit_memo.chomp
 
 CSV.open("#{file_name}.csv","a") do |csv|
-p "[#{memo}]"
+CSV.puts "[#{memo}]"
+
 end
 
-else 
-p "1か2の数値を入力してください"
+
+elsif memo_type == 3
+puts "アプリを終了します"
+
+
+else
+print "1,2,3のどれかの数値を入力してください"
 end
